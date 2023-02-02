8624bcd1bd9a54bfb4f496e77766d95dd4c03562
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-02-02 09:10:21 +0100

    Basics of angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,42 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	templateUrl: './app.component.html',
+	styleUrls: [
+		'./app.component.scss'
+	],
 })
+
 export class AppComponent {
-  title = 'angular-basics';
+  title = 'angular-basics';	
+	name = 'luis ramirez';
+	imgUrl = 'https://picsum.photos/id/237/500/500'
+	images = [
+		'https://picsum.photos/id/964/500/500',
+		'https://picsum.photos/id/236/500/500',
+		'https://picsum.photos/id/237/500/500'
+	]
+
+	currentDate = new Date();
+	currentAmount = 2000;
+	temperature = 25.3;
+	pizza = {
+		toppings: ['pepperoni', 'bacon'],
+		size: 'large'
+	};
+	blueClass = false;
+	fontSize = 16;
+
+	getName() {
+		return this.name;
+	}
+
+	changeImage(e: KeyboardEvent) {
+		this.imgUrl = (e.target as HTMLInputElement).value;
+	}
+
+	logImg(event: string) {
+		console.log(event);
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
@@ -14,3 +16,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
